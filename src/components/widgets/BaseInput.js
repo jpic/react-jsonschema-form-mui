2ec0b7a192c2e1a468b578eb69a377c92f7d0c4b
kousikpaul4u <kousikpaul4u@gmail.com>
2017-09-13 23:40:17 +0530

    Text field changed bootstrap to Material UI

--- src/components/widgets/BaseInput.js
+++ src/components/widgets/BaseInput.js
@@ -1,5 +1,7 @@
 import React from "react";
 import PropTypes from "prop-types";
+import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
+import TextField from "material-ui/TextField";
 
 function BaseInput(props) {
   // Note: since React 15.2.0 we can't forward unknown element attributes, so we
@@ -23,17 +25,21 @@ function BaseInput(props) {
     return props.onChange(value === "" ? options.emptyValue : value);
   };
   return (
-    <input
-      className="form-control"
-      readOnly={readonly}
-      disabled={disabled}
-      autoFocus={autofocus}
-      value={value == null ? "" : value}
-      {...inputProps}
-      onChange={_onChange}
-      onBlur={onBlur && (event => onBlur(inputProps.id, event.target.value))}
-      onFocus={onFocus && (event => onFocus(inputProps.id, event.target.value))}
-    />
+    <MuiThemeProvider>
+      <TextField
+        //className="form-control"
+        readOnly={readonly}
+        disabled={disabled}
+        autoFocus={autofocus}
+        value={value == null ? "" : value}
+        {...inputProps}
+        onChange={_onChange}
+        onBlur={onBlur && (event => onBlur(inputProps.id, event.target.value))}
+        onFocus={
+          onFocus && (event => onFocus(inputProps.id, event.target.value))
+        }
+      />
+    </MuiThemeProvider>
   );
 }
 
